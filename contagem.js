a55982bc13d253d9c44d066f3aaff8c0fe7b83d5
João Gustavo <jg789384@gmail.com>
2022-03-05 18:08:34 -0300

    contagem

---
+++ contagem.js
@@ -0,0 +1,15 @@
+let numeroAlunos = 10;
+
+for(let contador = 0; contador < numeroAlunos; contador++) {
+   // Resto % -> Retorna o valor de uma divisão inteira 
+    
+   if(contador  == 0) {
+       console.log ("O numero é ZERO")
+   } else if(contador % 2 == 0) {
+       //Concatenação
+       console.log('O numero ' + contador + ' é PAR')
+   } else if(contador % 2 == 1) {
+       //Interpolação
+       console.log(`O numero ${contador} é IMPAR`)
+   }
+   }
